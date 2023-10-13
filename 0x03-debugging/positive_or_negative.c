89aa812e0900458f3d8bff0818b035189f2fce02
K-kare <kamunawangari@gmail.com>
2023-10-13 06:32:45 -0700

    0-main

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,18 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-int main(void)
+#include <stdio.h>
+#include "main.h"
+/**
+*positive_or_negative - determine if number is positive or negative
+*@i: character to be checked
+*Return: void
+*/
+void positive_or_negative(int i)
 {
-	int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
 if (i > 0)
+{
 printf("%d is positive\n", i);
+}
 else if (i == 0)
+{
 printf("%d is zero\n", i);
+}
 else
+{
 printf("%d is negative\n", i);
+}
 }
