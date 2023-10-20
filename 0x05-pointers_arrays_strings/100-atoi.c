48c57fb35f3dee15889edd4dbd9dab320e9c8bd6
K-kare <kamunawangari@gmail.com>
2023-10-20 00:12:06 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_atoi - convert a string to an integer
+*@s: the pointer to convert
+*Return: a integer
+*/
+int _atoi(char *s)
+{
+int c = 0;
+unsigned int ni = 0;
+int min = 1;
+int isi =0;
+while (s[c])
+{
+if (s[c] == 45
+{
+min *= -1;
+}
+
+
